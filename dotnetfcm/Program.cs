0fc8870a77cfe3243b0ee01f2972945c12f86b86
cloudscope <cloudscopeph@gmail.com>
2019-05-09 16:31:59 +0800

    project template

---
+++ dotnetfcm/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace dotnetfcm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
